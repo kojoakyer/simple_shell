1039b18cedc3cdccc823494af1ad7a4bf1f5e759
Kojo Akyer <kojoakyer777@gmail.com>
2023-12-15 07:12:42 -0800

    updates

---
+++ executecmd.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void executecmd(char **argv){
+    char *command = NULL, *actual_command = NULL;
+
+    if (argv){
+        /* get the command */
+        command = argv[0];
+
+        /* generate the path to this command before passing it to execve */
+        actual_command = use_location(command);
+
+        /* execute the actual command with execve */
+        if (execve(actual_command, argv, NULL) == -1){
+            perror("Error:");
+        }
+    }
+    
+}
